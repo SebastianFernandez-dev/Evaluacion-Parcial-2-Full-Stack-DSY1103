c8d46aaddca723a8e31a2570c2077bee688dc35d
Harko018 <sebastian.fernandezsalinas16@gmail.com>
2026-05-16 22:54:15 -0400

    V1.4.1 Se hacen cambios en SucursalDTO-EmpleadoService-EmpleadoController

--- ms-empleados/src/main/java/com/dsy1103/msempleados/dto/SucursalDTO.java
+++ ms-empleados/src/main/java/com/dsy1103/msempleados/dto/SucursalDTO.java
@@ -4,9 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SucursalDTO {
-    private Long id; //Obtener los datos que necesito para la sucursal
+    private Long id;
+    private String nombre;
+    private String codigo;
+    private String direccion;
+    private Integer capacidadAtencion;
+    private Boolean activo;
+    private LocalDate fechaApertura;
+    private Long region;
 }
